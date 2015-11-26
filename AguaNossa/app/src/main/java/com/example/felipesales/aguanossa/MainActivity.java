b8f652721fed8abd4a7aac301fb0c7181a76b121
Felipe Sales <sales.felipeb@gmail.com>
2015-11-26 15:15:38 -0300

    Adicionando mensagens do web console ao locat

--- AguaNossa/app/src/main/java/com/example/felipesales/aguanossa/MainActivity.java
+++ AguaNossa/app/src/main/java/com/example/felipesales/aguanossa/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Build;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.*;
-
+import android.util.Log;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +19,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onStart() {
@@ -53,11 +33,18 @@ public class MainActivity extends ActionBarActivity {
             webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
             CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
         }
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+
+                Log.d("WebConsole", message + " -- From line "
+                        + lineNumber + " of "
+                        + sourceID);
+            }
+        });
 
         webView.loadUrl("file:///android_asset/index.html");
 
     }
-
-
 }
 
