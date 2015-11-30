8cd505512151d6b2dfbe9e570c7a1c28bbcad193
Felipe Sales <sales.felipeb@gmail.com>
2015-11-30 15:07:25 -0300

    Grafico de Voulume  adicionado

---
+++ AguaNossa/app/src/main/assets/js/app.js
@@ -0,0 +1,8 @@
+var config1 = liquidFillGaugeDefaultSettings();
+config1.circleColor = "#FF7777";
+config1.textColor = "#FF4444";
+config1.waveTextColor = "#FFAAAA";
+config1.circleThickness = 0.1;
+config1.textVertPosition = 0.5;
+config1.waveAnimateTime = 1000;
+var gauge2= loadLiquidFillGauge("graficoVolume", 13.8, config1);
