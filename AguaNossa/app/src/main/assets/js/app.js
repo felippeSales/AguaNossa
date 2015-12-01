8da019d8a4d6821b0f569fc809082ec280d109b4
Felipe Sales <sales.felipeb@gmail.com>
2015-12-01 10:00:36 -0300

    Adicionando Angular JS

--- AguaNossa/app/src/main/assets/js/app.js
+++ AguaNossa/app/src/main/assets/js/app.js
@@ -1,8 +1,14 @@
-var config1 = liquidFillGaugeDefaultSettings();
-config1.circleColor = "#FF7777";
-config1.textColor = "#FF4444";
-config1.waveTextColor = "#FFAAAA";
-config1.circleThickness = 0.1;
-config1.textVertPosition = 0.5;
-config1.waveAnimateTime = 1000;
-var gauge2= loadLiquidFillGauge("graficoVolume", 13.8, config1);
+var app = angular.module('AguaNossa', []);
+
+
+app.controller('GraficoVolume', function ($scope, $rootScope) {
+    $scope.chartConfig = liquidFillGaugeDefaultSettings();
+    $scope.chartConfig.circleColor = "#FF7777";
+    $scope.chartConfig.textColor = "#FF4444";
+    $scope.chartConfig.waveTextColor = "#FFAAAA";
+    $scope.chartConfig.circleThickness = 0.1;
+    $scope.chartConfig.textVertPosition = 0.5;
+    $scope.waveAnimateTime = 1000;
+    $scope.chart = loadLiquidFillGauge("graficoVolume", 13.8, $scope.chartConfig);
+    
+});
