27235cbb007870cfa57e783ec3c31c843504f2e6
Felipe Sales <sales.felipeb@gmail.com>
2016-02-18 15:16:36 -0300

    Resolvido o Bug da tela branca após clicar no twitter e voltar pra o app

--- AguaNossa/app/src/main/java/com/example/felipesales/aguanossa/MainActivity.java
+++ AguaNossa/app/src/main/java/com/example/felipesales/aguanossa/MainActivity.java
@@ -20,116 +20,112 @@ import android.content.Intent;
 
 public class MainActivity extends Activity {
 
-    /* URL saved to be loaded after fb login */
-    //private static final String target_url="http://felippesales.github.io/";
-    final String target_url="file:///android_asset/index.html";
-    private static final String target_url_prefix="www.example.com";
-    private Context mContext;
-    private WebView mWebview;
-    private WebView mWebviewPop;
-    private FrameLayout mContainer;
-    private long mLastBackPressTime = 0;
-    private Toast mToast;
+/* URL saved to be loaded after fb login */
+//private static final String target_url="http://felippesales.github.io/";
+final String target_url="file:///android_asset/index.html";
+private static final String target_url_prefix="www.example.com";
+private Context mContext;
+private WebView mWebview;
+private WebView mWebviewPop;
+private FrameLayout mContainer;
+private long mLastBackPressTime = 0;
+private Toast mToast;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        // final View controlsView =
-        // findViewById(R.id.fullscreen_content_controls);
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-        mWebview = (WebView) findViewById(R.id.webview);
-        //mWebviewPop = (WebView) findViewById(R.id.webviewPop);
-        mContainer = (FrameLayout) findViewById(R.id.webview_frame);
-        WebSettings webSettings = mWebview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setSupportMultipleWindows(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDisplayZoomControls(false);
-        mWebview.setWebViewClient(new UriWebViewClient());
-        mWebview.setWebChromeClient(new UriChromeClient());
+    setContentView(R.layout.activity_main);
+    // final View controlsView =
+    // findViewById(R.id.fullscreen_content_controls);
+    CookieManager cookieManager = CookieManager.getInstance();
+    cookieManager.setAcceptCookie(true);
+    mWebview = (WebView) findViewById(R.id.webview);
+    //mWebviewPop = (WebView) findViewById(R.id.webviewPop);
+    mContainer = (FrameLayout) findViewById(R.id.webview_frame);
+    WebSettings webSettings = mWebview.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    webSettings.setAppCacheEnabled(true);
+    webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+    webSettings.setSupportMultipleWindows(true);
+   // webSettings.setBuiltInZoomControls(true);
+    webSettings.setDisplayZoomControls(false);
+    mWebview.setWebViewClient(new UriWebViewClient());
+    mWebview.setWebChromeClient(new UriChromeClient());
+    mWebview.loadUrl(target_url);
+
+    mContext=this.getApplicationContext();
+
+}
+
+@Override
+public boolean onKeyDown(int keyCode, KeyEvent event) {
+    // Check if the key event was the Back button and if there's history
+    if (keyCode == KeyEvent.KEYCODE_BACK ) {
         mWebview.loadUrl(target_url);
+        return true;
+    }
+    // If it wasn't the Back key or there's no web page history, bubble up to the default
+    // system behavior (probably exit the activity)
+    return super.onKeyDown(keyCode, event);
+}
 
-        mContext=this.getApplicationContext();
 
+private class UriWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
+            Log.d("URl error", url);
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            view.getContext().startActivity(i);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // Check if the key event was the Back button and if there's history
-        if (keyCode == KeyEvent.KEYCODE_BACK ) {
-            mWebview.loadUrl(target_url);
-            return true;
-        }
-        // If it wasn't the Back key or there's no web page history, bubble up to the default
-        // system behavior (probably exit the activity)
-        return super.onKeyDown(keyCode, event);
+    public void onReceivedSslError(WebView view, SslErrorHandler handler,
+                                   SslError error) {
+        handler.proceed(); // Ignore SSL certificate errors
     }
+}
 
+class UriChromeClient extends WebChromeClient {
 
-    private class UriWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url != null && (url.startsWith("http://") || url.startsWith("https://"))) {
-                Log.d("URl error", url);
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                view.getContext().startActivity(i);
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler,
-                                       SslError error) {
-            handler.proceed(); // Ignore SSL certificate errors
-        }
-    }
-
-    class UriChromeClient extends WebChromeClient {
-
-        @Override
+            @Override
         public boolean onCreateWindow(WebView view, boolean isDialog,
-                                      boolean isUserGesture, Message resultMsg) {
+                                          boolean isUserGesture, Message resultMsg) {
 
-            mWebviewPop = new WebView(mContext);
+                mWebviewPop = new WebView(mContext);
+                mWebviewPop.setWebViewClient(new UriWebViewClient());
+                mWebviewPop.getSettings().setJavaScriptEnabled(true);
+                mWebviewPop.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                       ViewGroup.LayoutParams.MATCH_PARENT));
+                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                transport.setWebView(mWebviewPop);
+                resultMsg.sendToTarget();
 
-            mWebviewPop.setVerticalScrollBarEnabled(false);
-            mWebviewPop.setHorizontalScrollBarEnabled(false);
-            mWebviewPop.setWebViewClient(new UriWebViewClient());
-            mWebviewPop.getSettings().setJavaScriptEnabled(true);
-            mWebviewPop.getSettings().setSavePassword(false);
-            mWebviewPop.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT));
-            mContainer.addView(mWebviewPop);
-            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-            transport.setWebView(mWebviewPop);
-            resultMsg.sendToTarget();
 
-            Log.d("onOpenWindow", "called");
-            return true;
-        }
-
-        @Override
-        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-            Log.d("WebConsoleMessage", consoleMessage.message() + " -- From line "
-                    + consoleMessage.lineNumber() + " of "
-                    + consoleMessage.sourceId());
-
-            return true;
-        }
-
-        @Override
-        public void onCloseWindow(WebView window) {
-            Log.d("onCloseWindow", "called");
-        }
-
+        Log.d("onOpenWindow", "called");
+        return true;
     }
+
+    @Override
+    public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+        Log.d("WebConsoleMessage", consoleMessage.message() + " -- From line "
+                + consoleMessage.lineNumber() + " of "
+                + consoleMessage.sourceId());
+
+        return true;
+    }
+
+    @Override
+    public void onCloseWindow(WebView window) {
+        Log.d("onCloseWindow", "called");
+    }
+
+}
 }
